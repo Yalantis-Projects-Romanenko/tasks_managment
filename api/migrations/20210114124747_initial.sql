b9dd337f302e542011d96287e0b1afb51916269d
fdistorted <andrejjrom@gmail.com>
2021-01-21 19:19:37 +0200

    migrations values fixed

--- api/migrations/20210114124747_initial.sql
+++ api/migrations/20210114124747_initial.sql
@@ -14,9 +14,9 @@ CREATE TABLE IF NOT EXISTS projects (
     PRIMARY KEY (id)
     );
 
-CREATE TABLE IF NOT EXISTS columns (
+CREATE TABLE IF NOT EXISTS lists (
     id UUID DEFAULT uuid_generate_v4(),
-    project_id varchar NOT NULL,
+    project_id UUID NOT NULL,
     cname varchar NOT NULL ,
     index integer ,
 
@@ -27,6 +27,7 @@ CREATE TABLE IF NOT EXISTS columns (
 
 CREATE TABLE IF NOT EXISTS tasks (
     id UUID DEFAULT uuid_generate_v4(),
+    column_id UUID NOT NULL,
     title varchar NOT NULL,
     description varchar NOT NULL,
     priority integer,
@@ -38,6 +39,7 @@ CREATE TABLE IF NOT EXISTS tasks (
 
 CREATE TABLE IF NOT EXISTS comments (
     id UUID DEFAULT uuid_generate_v4(),
+    task_id UUID NOT NULL,
     username varchar NOT NULL,
     content varchar NOT NULL,
 
@@ -51,6 +53,6 @@ CREATE TABLE IF NOT EXISTS comments (
 -- +goose Down
 -- SQL in this section is executed when the migration is rolled back.
 DROP TABLE projects;
-DROP TABLE columns;
+DROP TABLE lists;
 DROP TABLE tasks;
 DROP TABLE comments;
