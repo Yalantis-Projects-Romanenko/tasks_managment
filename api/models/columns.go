5381870a9c48639d7bf0b9607e47221226a38624
fdistorted <andrejjrom@gmail.com>
2021-01-21 19:20:12 +0200

    more validation parameters added

--- api/models/columns.go
+++ api/models/columns.go
@@ -3,9 +3,9 @@ package models
 import "time"
 
 type Column struct {
-	Id             string    `json:"id"`
-	ProjectId      string    `json:"projectId"`
-	Name           string    `json:"name"`
-	IndexInProject string    `json:"indexInProject"`
-	Created        time.Time `json:"created"`
+	Id        string    `json:"id"`
+	ProjectId string    `json:"projectId,omitempty"`
+	Name      string    `json:"name" validate:"required, gte=3"`
+	Index     int64     `json:"index"` // position in project
+	Created   time.Time `json:"created"`
 }
--- api/models/projects.go
+++ api/models/projects.go
@@ -4,8 +4,8 @@ import "time"
 
 type Project struct {
 	Id          string    `json:"id"`
-	UserId      string    `json:"userId"`
-	Name        string    `json:"name" validate:"required"`
-	Description string    `json:"description" validate:"required"`
+	UserId      string    `json:"userId,omitempty"`
+	Name        string    `json:"name" validate:"required, gte=3"`
+	Description string    `json:"description" validate:"required, gte=5"`
 	Created     time.Time `json:"created"`
 }
